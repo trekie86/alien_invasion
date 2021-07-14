c60c8f13ab394ae832c7338fdc4fe8099be2e0c2
Rob Wolinski <rwolinsk@amazon.com>
2021-07-13 17:31:40 -0700

    start defining Alien class

---
+++ alien.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """A class to represent a single alien in the fleet."""
+
+    def __init__(self, ai_game):
+        """Initialize the alien and set its starting position."""
+        super().__init__()
+        self.screen = ai_game.screen
