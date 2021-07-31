fa5dd7ed908577006cd9909e0f850b6c93db52cf
Rob Wolinski <rwolinsk@amazon.com>
2021-07-31 11:07:57 -0700

    The aliens move left, right, and down now

--- alien.py
+++ alien.py
@@ -9,6 +9,7 @@ class Alien(Sprite):
         """Initialize the alien and set its starting position."""
         super().__init__()
         self.screen = screen
+        self.settings = settings
 
         # Load the alien image and set it's rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
@@ -20,3 +21,14 @@ class Alien(Sprite):
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien to the right or left."""
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
+
+    def check_edges(self):
+        """Return true if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
--- alien_invasion.py
+++ alien_invasion.py
@@ -39,6 +39,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -121,6 +122,25 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien_size[1] + 2 * alien_size[1] * row_number
         self.aliens.add(alien)
+
+    def _update_aliens(self):
+        """Check if the fleet is at an edge,
+            then update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
 if __name__ == '__main__':
     ai = AlienInvasion()
